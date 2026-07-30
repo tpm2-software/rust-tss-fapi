3e969ae2ee027b037c575e82fbfa68bf87b87ca2
Daniel Trick <daniel.trick@sit.fraunhofer.de>
2026-07-30 17:13:15 +0200

    Updated Docker image 'rust-tss2-docker' to the latest version.

--- tools/docker/build/rstss.mk
+++ tools/docker/build/rstss.mk
@@ -1,9 +1,9 @@
 ifeq ($(filter-out stable,$(RSTSS_BRANCH)),)
-  export IMAGE_VERSION_RSTSS := sha256:50feada2f78d3393c151b4d49d639ba2c6518b2cadf76c49e5fe8977b89c9fa4
+  export IMAGE_VERSION_RSTSS := sha256:276c6d4e9510ede4a2e868e0158e7ae79b99393656f889670da8ce4dc44746e4
 else ifeq ($(RSTSS_BRANCH),unstable)
   export IMAGE_VERSION_RSTSS := sha256:c8464bf6cb191194a226bef0e4d9fea90b6d51ba5b05417e01a3cf2e543c6228
 else ifeq ($(RSTSS_BRANCH),bleeding-edge)
-  export IMAGE_VERSION_RSTSS := sha256:1e51c7bbeefc11c838e796cc2bdc8d69f2a7063893680e88c7a2da9e1ed9fdb7
+  export IMAGE_VERSION_RSTSS := sha256:58066d0ad1ab47fb4e60e52bfe90ea59f8a5bbb5c6c011e2e3523c2c6b1dfa63
 else
   $(error Unsupport RSTSS_BRANCH branch "$(RSTSS_BRANCH)" specified!)
 endif
